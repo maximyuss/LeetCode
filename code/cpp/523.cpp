c04bfb6edcdd74413618c6dab22aa1ee6108cfe5
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2024-06-08 13:59:18 +0200

    Update 523.cpp

--- code/cpp/523.cpp
+++ code/cpp/523.cpp
@@ -1,17 +1,15 @@
 //https://leetcode.com/problems/continuous-subarray-sum/
 bool checkSubarraySum(vector<int>& nums, int k) {
-	unordered_map<int, int> mp;
-	mp[0] = -1;
-	size_t sum = 0, rem;
-	for (int i = 0; i < nums.size(); i++) {
-		sum += nums[i];
-		rem = sum % k;
-		if (mp.contains(rem)) {
-			if (i - mp[rem] > 1)
-				return true;
-		}
-		else
-			mp[rem] = i;
-	}
-	return false;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        size_t sum = 0, rem;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            rem = sum % k;
+            if (!mp.contains(rem))
+                mp[rem] = i;
+            else if (i - mp[rem] > 1)
+                return true;
+        }
+        return false;
 }
