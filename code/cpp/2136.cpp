f104bce5688f67b87264fd8985b7fd5dc767927d
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2024-06-19 12:55:47 +0200

    Create 2136.cpp

---
+++ code/cpp/2136.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/earliest-possible-day-of-full-bloom/
+class Solution {
+public:
+    Solution() {ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);}
+    
+    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+        size_t n = plantTime.size(), totalPlantTime = -1, totalMaxTime = 0;
+        vector <pair<int, size_t>> pairs (n);
+        for (size_t i = 0; i < n; i++) {
+            pairs[i] = { growTime[i], plantTime[i] };
+        }
+        sort(pairs.begin(), pairs.end());
+        for (size_t i = n - 1; i + 1 != 0; i--) {
+            totalPlantTime += pairs[i].second;
+            totalMaxTime = max(totalMaxTime, totalPlantTime + pairs[i].first);
+        }
+        return totalMaxTime + 1;
+    }
+};
