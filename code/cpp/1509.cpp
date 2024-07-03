cd6d1a043d8388b2cc7b4338a8f3b788972bf2b7
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2024-07-03 15:10:44 +0200

    Update 1509.cpp

--- code/cpp/1509.cpp
+++ code/cpp/1509.cpp
@@ -32,19 +32,36 @@ public:
             }
         return res;
     }
-
+    
+    // First version - using my function
     int minDifference(vector<int>& nums) {
-        if (nums.size() < 5)
-            return 0;
-        vector<int> arr, tmp;
-        int res = INT_MAX;
-        arr = findNMinsOrMaxs(MIN, nums, 4);
-        tmp = findNMinsOrMaxs(MAX, nums, 4);
-        for (int num : tmp)
-            if (num > arr[3])
-                arr.push_back(num);
-        for (int l = 0, r = arr.size() - 4; l < 4; l++, r++)
-            res = min(res, arr[r] - arr[l]);
-        return res;
+        if (nums.size() < 5) return 0;
+        
+        vector<int> small = findNMinsOrMaxs(MIN, nums, 4);
+        vector<int> big = findNMinsOrMaxs(MAX, nums, 4);
+        
+        int min_diff = INT_MAX;
+        for (int i = 0; i <= 3; ++i)
+            min_diff = min(min_diff, big[i] - small[i]);
+        return min_diff;
+    }
+
+    // Second version - using nth_element
+    int minDifference_v2(vector<int>& nums) {
+    	int n = nums.size();
+    	if (n < 5) return 0;
+        
+    	nth_element(nums.begin(), nums.begin() + 4, nums.end());
+    	vector<int> small(nums.begin(), nums.begin() + 4);
+    	sort(small.begin(), small.end());
+        
+    	nth_element(nums.begin(), nums.begin() + 4, nums.end(), greater<int>());
+    	vector<int> big(nums.begin(), nums.begin() + 4);
+    	sort(big.begin(), big.end());
+    
+    	int min_diff = INT_MAX;
+    	for (int i = 0; i <= 3; ++i)
+    		min_diff = min(min_diff, big[i] - small[i]);
+    	return min_diff;
     }
 };
