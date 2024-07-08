4c49a7e8b7c063945af4b3c54d1782319d00bc62
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2024-07-08 10:55:24 +0200

    Create 1823.py

---
+++ code/python/1823.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        res = 0
+        for i in range(2, n + 1):
+            res = (res + k) % i
+        return res + 1
