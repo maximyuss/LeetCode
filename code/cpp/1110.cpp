30a781d14b2e80f992f1ea59d9fb291d8d995cd9
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2024-07-17 14:18:17 +0200

    Create 1110.cpp

---
+++ code/cpp/1110.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/delete-nodes-and-return-forest/
+
+class Solution {
+public:
+    void dfs(TreeNode* curr, const set<int>& st, vector<TreeNode*>& res, TreeNode* parent, bool isLeft, bool isNew = false) {
+        if (!curr) return;
+        if (st.contains(curr->val)) {
+            if (parent) {
+                if (isLeft)
+                    parent->left = NULL;
+                else
+                    parent->right = NULL;
+                isNew = true;
+            }
+            dfs(curr->left, st, res, curr, true, isNew);
+            dfs(curr->right, st, res, curr, false, isNew);
+        }
+        else {
+            if (isNew) {
+                res.emplace_back(curr);
+                isNew = false;
+            }
+            dfs(curr->left, st, res, curr, true);
+            dfs(curr->right, st, res, curr, false);
+        }
+    }
+
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        vector<TreeNode*> res;
+        res.reserve(1000);
+        set<int> st(to_delete.begin(), to_delete.end());
+        dfs(root, st, res, NULL, true, true);
+        return res;
+    }
+};
