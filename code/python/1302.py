7b5c90626d0de7e81e640016f810ecbc77406c52
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2024-12-20 17:34:30 +0100

    Create 1302.py

---
+++ code/python/1302.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/deepest-leaves-sum/
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        next_level = deque([root])
+        while next_level:
+            curr_level = next_level
+            next_level = deque()
+            for node in curr_level:
+                if node.left: next_level.append(node.left)
+                if node.right: next_level.append(node.right)
+        return sum([node.val for node in curr_level])
