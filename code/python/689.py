c6ec943e55c1676054ac0387139265038bf4f594
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2024-12-30 08:11:19 +0100

    Update 689.py

--- code/python/689.py
+++ code/python/689.py
@@ -1,11 +1,13 @@
-# https://leetcode.com/problems/maximum-sum-of-3-non-overlapping-subarrays/
 class Solution:
+
     def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
         if n == 3 * k: return [0, k, 2 * k]
         sums = [0] * (n - k + 1)
         cnt = k
         sum_window = j = 0
+        
+        # Calculating subarray sums
         for i in range(n):
             if cnt > 0:
                 sum_window += nums[i]
@@ -14,6 +16,8 @@ class Solution:
                 sums[j] = sum_window
                 sum_window += nums[i] - nums[j]
                 j += 1
+        
+        # Calculating right maxima
         sums[j] = sum_window
         prefix = [-1] * (n - k)
         j = n - k
@@ -23,6 +27,8 @@ class Solution:
                 cur_max = (j, sums[j])
             prefix[i] = cur_max[0]
             j -= 1
+        
+        # Calculating left maxima
         suffix = [-1] * (n - k)
         j = 0
         cur_max = (0, sums[0])
@@ -31,6 +37,8 @@ class Solution:
                 cur_max = (j, sums[j])
             suffix[i] = cur_max[0]
             j += 1
+        
+        # Searching for the answer
         cur_max=(-1, -1)
         for i in range(k, n - 2 * k + 1):
             sum_ = sums[i] + sums[prefix[i]] + sums[suffix[i]]
