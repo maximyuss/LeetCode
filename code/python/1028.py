35ef7b975e8af517f3f93d2103e0849331f232a6
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2025-02-22 12:26:06 +0100

    Update 1028.py

--- code/python/1028.py
+++ code/python/1028.py
@@ -1,37 +1,17 @@
 # https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/
 class Solution:
     def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
-        def parse(s):
-            res = deque()
-            l = is_node = 0
-            for r in range(len(s)):
-                if is_node ^ (s[r] == '-'): continue
-                if is_node:
-                    val = int(s[l:r])
-                else:
-                    level = r - l
-                if is_node:
-                    res.append((level, val))
-                is_node = not is_node
-                l = r
-            return res
+        def dfs(i=0, depth=0):
+            if i == n or traversal[i:i + depth] != "-" * depth:
+                return None, i
+            i += depth
+            j = i
+            while j < n and traversal[j] != "-":
+                j += 1
+            node = TreeNode(int(traversal[i:j]))
+            node.left, i = dfs(j, depth + 1)
+            node.right, i = dfs(i, depth + 1)
+            return node, i
 
-        def dfs(node, parent_level):
-            if not node or not nodes: return
-            level, val = nodes[0]
-            if level <= parent_level: return
-            node.left = TreeNode(val)
-            nodes.popleft()
-            dfs(node.left, level)
-            
-            if not nodes: return
-            level, val = nodes[0]
-            if level <= parent_level: return
-            node.right = TreeNode(val)
-            nodes.popleft()
-            dfs(node.right, level)
-
-        nodes = parse(traversal + '-')
-        root = TreeNode(nodes.popleft()[1])
-        dfs(root, 0)
-        return root
+        n = len(traversal)
+        return dfs()[0]
