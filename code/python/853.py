02acbe9e8306e66c82f9fb44d4118268fa3b9927
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2025-02-23 13:34:53 +0100

    Create 853.py

---
+++ code/python/853.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/car-fleet/
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = deque()
+        for pos, v in sorted(zip(position, speed),reverse = True):
+            dist = target - pos
+            time = dist / v 
+            if not stack:
+                stack.append(time)
+            elif time > stack[-1]:
+                stack.append(time)
+        return len(stack)
