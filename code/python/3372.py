cff5c93a0506b34d2b3f55d345a9d139a35aae97
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2025-05-29 19:11:59 +0200

    Update 3372.py

--- code/python/3372.py
+++ code/python/3372.py
@@ -1,26 +1,29 @@
 # https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i/
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
+        def build_graph(edges):
+            n = len(edges) + 1
+            graph = [[] for _ in range(n)]
+            for a, b in edges:
+                graph[a].append(b)
+                graph[b].append(a)
+            return graph
+
         def dfs(node, level, par, count=1) -> int:
             if level < k:
-                for child in graph[node]:
-                    if child != par:
-                        count += dfs(child, level + 1, node)
+                for neighbor in graph[node]:
+                    if neighbor != par:
+                        count += dfs(neighbor, level + 1, node)
             return count
 
-        m, n = len(edges1) + 1, len(edges2) + 1
+        m = len(edges1) + 1
         max_connection_graph2 = 0
-        if k == 0: return [1] * m
-        graph = [[] for _ in range(n)]
-        for u, v in edges2:
-            graph[u] += v,
-            graph[v] += u,
-        for node in range(n):
+        if k == 0:
+            return [1] * m
+        graph = build_graph(edges2)
+        for node in range(len(edges2) + 1):
             max_connection_graph2 = max(max_connection_graph2, dfs(node, 1, -1))
-        graph = [[] for _ in range(m)]
-        for u, v in edges1:
-            graph[u] += v,
-            graph[v] += u,
+        graph = build_graph(edges1)
         res = [0] * m
         for i in range(m):
             res[i] = dfs(i, 0, -1) + max_connection_graph2
