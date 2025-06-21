1e69e32d35fe239c26d707ca320bb15cbc8a2bd9
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2025-06-21 21:08:59 +0200

    Create 2216.java

---
+++ code/java/2216.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/minimum-deletions-to-make-array-beautiful/
+class Solution {
+    public int minDeletions(String s) {
+        int[] freq = new int[26];
+        for (char ch: s.toCharArray())
+            freq[ch - 'a']++;
+        HashSet<Integer> seen = new HashSet<>();
+        int res = 0;
+        for (int f: freq) {
+            if (f == 0) continue;
+            while (f > 0 && seen.contains(f)) {
+                f--;
+                res++;
+            }
+            seen.add(f);
+        }
+        return res;
+    }
+}
