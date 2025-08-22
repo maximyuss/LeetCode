5db62c03bf8ed19e526e63897f81a62cfad6b91f
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2025-08-22 10:09:14 +0200

    Update 3195.py

--- import/new/3195.py
+++ import/new/3195.py
@@ -1,32 +1,12 @@
 # https://leetcode.com/problems/find-the-minimum-area-to-cover-all-ones-i
-class Solution {
-    public int minimumArea(int[][] grid) {
-        int n = grid.length, m = grid[0].length;    
-        int y0 = 0;
-        for (; y0 < n; y0++) {
-            int[] r = grid[y0];
-            int j = 0; for (; j < m && r[j] == 0; j++);
-            if (j < m) break;
-        }
-        if (y0 == n) return 0;
-        int y1 = n - 1;
-        for (;; y1--) {
-            int[] r = grid[y1];
-            int j = 0; for (; j < m && r[j] == 0; j++);
-            if (j < m) break;
-        }
-        int x0 = 0;
-        outer0:
-        for (; x0 < m; x0++) {
-            for (int i = y0; i <= y1; i++) 
-                if (grid[i][x0] != 0) break outer0;
-        }
-        int x1 = m - 1;
-        outer1:
-        for (; x1 >= 0; x1--) {
-            for (int i = y0; i <= y1; i++) 
-                if (grid[i][x1] != 0) break outer1;
-        }
-        return (x1 - x0 + 1) * (y1 - y0 + 1);
-    }
-}
+class Solution:
+    def minimumArea(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        try:
+            y0 = next(i for i in range(n) if 1 in grid[i])
+            y1 = next(i for i in range(n - 1, -1, -1) if 1 in grid[i])
+        except StopIteration:
+            return 0
+        x0 = next(j for j in range(m) if any(grid[i][j] for i in range(y0, y1 + 1)))
+        x1 = next(j for j in range(m - 1, -1, -1) if any(grid[i][j] for i in range(y0, y1 + 1)))
+        return (x1 - x0 + 1) * (y1 - y0 + 1)
