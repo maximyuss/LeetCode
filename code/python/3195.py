d8564e914ca1bd0657383e15b94352365fdf49d1
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2025-08-22 10:14:14 +0200

    Update 3195.py

--- code/python/3195.py
+++ code/python/3195.py
@@ -2,11 +2,9 @@
 class Solution:
     def minimumArea(self, grid: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
-        try:
-            y0 = next(i for i in range(n) if 1 in grid[i])
-            y1 = next(i for i in range(n - 1, -1, -1) if 1 in grid[i])
-        except StopIteration:
-            return 0
-        x0 = next(j for j in range(m) if any(grid[i][j] for i in range(y0, y1 + 1)))
-        x1 = next(j for j in range(m - 1, -1, -1) if any(grid[i][j] for i in range(y0, y1 + 1)))
+        y0 = next((i for i in range(n) if 1 in grid[i]), None)
+        if y0 is None: return 0
+        y1 = next((i for i in range(n - 1, -1, -1) if 1 in grid[i]), None)
+        x0 = next((j for j in range(m) if any(grid[i][j] for i in range(y0, y1 + 1))), None)
+        x1 = next((j for j in range(m - 1, -1, -1) if any(grid[i][j] for i in range(y0, y1 + 1))), None)
         return (x1 - x0 + 1) * (y1 - y0 + 1)
