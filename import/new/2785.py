81e181b460e9e9eccff1bda72a72cc32f480aed6
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2025-09-11 14:08:51 +0200

    Update 2785.py

--- import/new/2785.py
+++ import/new/2785.py
@@ -1,11 +1,11 @@
 # https://leetcode.com/problems/sort-vowels-in-a-string
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vowels_order = "AEIOUaeiou"
+        vowels = "AEIOUaeiou"
         counts = Counter(s)
-        for v in vowels_order:
+        for v in vowels:
             s = s.replace(v, '_')
-        for v in vowels_order:
+        for v in vowels:
             cnt = counts.get(v, 0)
             if cnt:
                 s = s.replace('_', v, cnt)
